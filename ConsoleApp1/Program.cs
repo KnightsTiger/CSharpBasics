fe10b8cef38ce39ba2c3aafd429ee26f230b9db9
Knight <Knight@git.com>
2018-12-16 13:23:12 +0530

    Basics added

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ApplicationValue = 10;
+            Console.Write(10);
+            ApplicationValue = 20;
+            Console.Write(10);
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine();
+            if(ApplicationValue == 10)
+            {
+                Console.WriteLine("yes" + ApplicationValue);
+            }
+
+
+
+
+            //Array
+            int [] arrayItems = new int[5];
+            arrayItems[0] = 1;
+            arrayItems[1] = 12;
+            arrayItems[2] = 41;
+            arrayItems[3] = 16;
+            //For each
+            foreach (int item in arrayItems)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
